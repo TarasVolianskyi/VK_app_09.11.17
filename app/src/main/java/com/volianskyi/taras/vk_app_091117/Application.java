370906f73107b53e54b469272f8922cad86c8e60
TarasVolianskyi <volianskyitv@gmail.com>
2017-11-09 22:06:16 +0200

    add authorusation

--- app/src/main/java/com/volianskyi/taras/vk_app_091117/Application.java
+++ app/src/main/java/com/volianskyi/taras/vk_app_091117/Application.java
@@ -1,8 +1,17 @@
 package com.volianskyi.taras.vk_app_091117;
 
+import com.vk.sdk.VKSdk;
+
 /**
  * Created by tarasvolianskyi on 09.11.17.
  */
 
-public class Application {
+public class Application extends android.app.Application{
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        VKSdk.initialize(this);
+    }
 }
--- app/src/main/java/com/volianskyi/taras/vk_app_091117/MainActivity.java
+++ app/src/main/java/com/volianskyi/taras/vk_app_091117/MainActivity.java
@@ -1,20 +1,48 @@
 package com.volianskyi.taras.vk_app_091117;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
 
+import com.vk.sdk.VKAccessToken;
+import com.vk.sdk.VKCallback;
+import com.vk.sdk.VKScope;
+import com.vk.sdk.VKSdk;
+import com.vk.sdk.api.VKError;
 import com.vk.sdk.util.VKUtil;
 
 import java.sql.Array;
 import java.util.Arrays;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
+
+    private String[] scope = new String[]{VKScope.MESSAGES,VKScope.FRIENDS,VKScope.WALL};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        String[] fingerprints = VKUtil.getCertificateFingerprint(this, this.getPackageName());
-        System.out.println(Arrays.asList(fingerprints));
+        //String[] fingerprints = VKUtil.getCertificateFingerprint(this, this.getPackageName());
+        //System.out.println(Arrays.asList(fingerprints));
+        VKSdk.login(this);
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (!VKSdk.onActivityResult(requestCode, resultCode, data, new VKCallback<VKAccessToken>() {
+            @Override
+            public void onResult(VKAccessToken res) {
+                Toast.makeText(getApplicationContext(),"Good",Toast.LENGTH_LONG).show();
+            }
+            @Override
+            public void onError(VKError error) {
+                Toast.makeText(getApplicationContext(),"Error",Toast.LENGTH_LONG).show();
+            }
+        })) {
+            super.onActivityResult(requestCode, resultCode, data);
+        }
     }
 }
